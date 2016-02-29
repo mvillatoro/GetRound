7fb0ada9af23ac01e61effaa075fa3d6614a513c
Mario V <mvilla220@gmail.com>
2016-02-28 18:05:40 -0600

    board

--- getRound/getRound.ino
+++ getRound/getRound.ino
@@ -3,8 +3,37 @@
 #include "stdio.h"
 #include "time.h"
 
-unsigned char wallabove[] ={
+unsigned char wallAboveBelow[] ={
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
 GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN
+};
+
+unsigned char wallLeftRight[] ={
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN
 };
 
 struct Sprite
@@ -17,28 +46,59 @@ struct Sprite
 };
 
 struct Sprite WallAbove;
+struct Sprite WallBelow;
+struct Sprite WallLeft;
+struct Sprite WallRight;
 
 void InitWallAbove(){
-  WallAbove.posX = 0;
-  WallAbove.PosY = 0;
-  WallAbove.width = 10;  
+  WallAbove.posX = 1;
+  WallAbove.posY = 1;
+  WallAbove.width = 158;  
   WallAbove.height = 1;
-  WallAbove.image = wallabove;
+  WallAbove.image = wallAboveBelow;
 };
 
-void initData
-{
-  InitWallAbove();
-  
-    VGA.begin(VGAWISHBONESLOT(9),CHARMAPWISHBONESLOT(10));
-  Serial.begin(9600);
-  VGA.setBackgroundColor(BLACK);
-  
-  VGA.writeArea(WallAbove.posX,WallAbove.posY,WallAbove.width, WallAbove.height, WallAbove.image);
+void InitWallBelow(){
+  WallBelow.posX = 1;
+  WallBelow.posY = 117;
+  WallBelow.width = 158;  
+  WallBelow.height = 1;
+  WallBelow.image = wallAboveBelow;
+};
+
+void InitWallLeft(){
+  WallLeft.posX = 1;
+  WallLeft.posY = 1;
+  WallLeft.width = 1;  
+  WallLeft.height = 117;
+  WallLeft.image = wallLeftRight;
+};
+
+void InitWallRight(){
+  WallRight.posX = 150;
+  WallRight.posY = 1;
+  WallRight.width = 1;  
+  WallRight.height = 117;
+  WallRight.image = wallLeftRight;
 };
 
 void setup()
 { 
+  
+  InitWallAbove();
+  InitWallBelow();
+  InitWallLeft();
+  InitWallRight();
+  
+  VGA.begin(VGAWISHBONESLOT(9),CHARMAPWISHBONESLOT(10));
+  Serial.begin(9600);
+  VGA.setBackgroundColor(BLACK);
+  
+  VGA.writeArea(WallAbove.posX,WallAbove.posY,WallAbove.width, WallAbove.height, WallAbove.image);
+  VGA.writeArea(WallBelow.posX,WallBelow.posY,WallBelow.width, WallBelow.height, WallBelow.image);
+  VGA.writeArea(WallLeft.posX,WallLeft.posY,WallLeft.width, WallLeft.height, WallLeft.image);
+  VGA.writeArea(WallRight.posX,WallRight.posY,WallRight.width, WallRight.height, WallRight.image);
+  //VGA.clear();
 };
 
 void loop()

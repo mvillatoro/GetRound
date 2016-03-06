e91aad3e106950dd27e9d0b0300f49b136395b07
Mario V <mvilla220@gmail.com>
2016-03-06 17:48:23 -0600

    brute force add 2

--- getRound/collision.h
+++ getRound/collision.h
@@ -8,6 +8,17 @@ struct block{
   unsigned char *image;
 };
 
+
+unsigned char enter_Door[]={
+  RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED,
+  RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED, RED
+};
+
+unsigned char exit_Door[]={
+  GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN,
+  GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN, GREEN
+};
+
 unsigned char player1[]={
 BLACK,BLACK,WHITE,WHITE,BLACK,
 BLACK,BLACK,BLACK,WHITE,BLACK,
@@ -59,6 +70,19 @@ int PointInRectangle(int x,int y,int x1,int y1,int x2,int y2){
         return ((( (x) >= (x1)) && ((y) >= (y1))) && (((x) <= (x2)) && ((y) <= (y2))));
 };
 
+
+void print_text(int x, int y, char * msg){
+      VGA.setColor(WHITE);
+      VGA.printtext(x, y, msg);
+}
+
+void print_number(int x, int y, int msg){
+      char snum[5];
+      itoa (msg, snum, 10);
+      VGA.setColor(WHITE);
+      VGA.printtext(x, y, snum);
+}
+
 int Inteaction(block s1, block s2)
 {
         return (PointInRectangle(s1.posX+1,            s1.posY+1,               s2.posX+1, s2.posY+1,    s2.posX + s2.width-1,             s2.posY + s2.height-1)  ||
@@ -83,6 +107,8 @@ void init_block(block* _block, int type, int x, int y, int width, int height, in
     case 1 : _block->image = pixel; break;
     case 2 : _block->image = player1; break;
     case 3 : _block->image = walls; break;
+    case 4 : _block->image = exit_Door; break;
+    case 5 : _block->image = enter_Door; break;
     default : break;
   }
   _block->active = active;
